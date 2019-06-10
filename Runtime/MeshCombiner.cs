175dfdb3ff86dd1cf6f095c1526e75df4997a10d
Mattias Edlund <matte.edlund@gmail.com>
2019-06-11 01:45:21 +0900

    Created a WIP mesh combiner.

---
+++ Runtime/MeshCombiner.cs
@@ -0,0 +1,210 @@
+﻿#region License
+/*
+MIT License
+
+Copyright(c) 2019 Mattias Edlund
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion
+
+using UnityEngine;
+
+namespace UnityMeshSimplifier
+{
+    /// <summary>
+    /// Contains methods for combining meshes.
+    /// </summary>
+    public static class MeshCombiner
+    {
+        #region Public Methods
+        /// <summary>
+        /// Combines an array of mesh renderers into one single mesh.
+        /// </summary>
+        /// <param name="rootTransform">The root transform to create the combine mesh based from, essentially the origin of the new mesh.</param>
+        /// <param name="renderers">The array of mesh renderers to combine.</param>
+        /// <param name="resultMaterials">The resulting materials for the combined mesh.</param>
+        /// <returns>The combined mesh.</returns>
+        public static Mesh CombineMeshes(Transform rootTransform, MeshRenderer[] renderers, out Material[] resultMaterials)
+        {
+            if (rootTransform == null)
+                throw new System.ArgumentNullException(nameof(rootTransform));
+            else if (renderers == null)
+                throw new System.ArgumentNullException(nameof(renderers));
+
+            var meshes = new Mesh[renderers.Length];
+            var transforms = new Matrix4x4[renderers.Length];
+            var materials = new Material[renderers.Length][];
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                var renderer = renderers[i];
+                if (renderer == null)
+                    throw new System.ArgumentException(string.Format("The renderer at index {0} is null.", i), nameof(renderers));
+
+                var rendererTransform = renderer.transform;
+                var meshFilter = renderer.GetComponent<MeshFilter>();
+                if (meshFilter == null)
+                    throw new System.ArgumentException(string.Format("The renderer at index {0} has no mesh filter.", i), nameof(renderers));
+                else if (meshFilter.sharedMesh == null)
+                    throw new System.ArgumentException(string.Format("The mesh filter for renderer at index {0} has no mesh.", i), nameof(renderers));
+
+                meshes[i] = meshFilter.sharedMesh;
+                transforms[i] = rootTransform.worldToLocalMatrix * rendererTransform.localToWorldMatrix;
+                materials[i] = renderer.sharedMaterials;
+            }
+
+            return CombineMeshes(meshes, transforms, materials, out resultMaterials);
+        }
+
+        /// <summary>
+        /// Combines an array of skinned mesh renderers into one single skinned mesh.
+        /// </summary>
+        /// <param name="rootTransform">The root transform to create the combine mesh based from, essentially the origin of the new mesh.</param>
+        /// <param name="renderers">The array of skinned mesh renderers to combine.</param>
+        /// <param name="resultMaterials">The resulting materials for the combined mesh.</param>
+        /// <param name="resultBones">The resulting bones for the combined mesh.</param>
+        /// <returns>The combined mesh.</returns>
+        public static Mesh CombineMeshes(Transform rootTransform, SkinnedMeshRenderer[] renderers, out Material[] resultMaterials, out Transform[] resultBones)
+        {
+            if (rootTransform == null)
+                throw new System.ArgumentNullException(nameof(rootTransform));
+            else if (renderers == null)
+                throw new System.ArgumentNullException(nameof(renderers));
+
+            var meshes = new Mesh[renderers.Length];
+            var transforms = new Matrix4x4[renderers.Length];
+            var materials = new Material[renderers.Length][];
+            var bones = new Transform[renderers.Length][];
+
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                var renderer = renderers[i];
+                if (renderer == null)
+                    throw new System.ArgumentException(string.Format("The renderer at index {0} is null.", i), nameof(renderers));
+                else if (renderer.sharedMesh == null)
+                    throw new System.ArgumentException(string.Format("The renderer at index {0} has no mesh.", i), nameof(renderers));
+
+                var rendererTransform = renderer.transform;
+                meshes[i] = renderer.sharedMesh;
+                transforms[i] = rootTransform.worldToLocalMatrix * rendererTransform.localToWorldMatrix;
+                materials[i] = renderer.sharedMaterials;
+                bones[i] = renderer.bones;
+            }
+
+            return CombineMeshes(meshes, transforms, materials, bones, out resultMaterials, out resultBones);
+        }
+
+        /// <summary>
+        /// Combines an array of meshes into a single mesh.
+        /// </summary>
+        /// <param name="meshes">The array of meshes to combine.</param>
+        /// <param name="transforms">The array of transforms for the meshes.</param>
+        /// <param name="materials">The array of materials for each mesh to combine.</param>
+        /// <param name="resultMaterials">The resulting materials for the combined mesh.</param>
+        /// <returns>The combined mesh.</returns>
+        public static Mesh CombineMeshes(Mesh[] meshes, Matrix4x4[] transforms, Material[][] materials, out Material[] resultMaterials)
+        {
+            if (meshes == null)
+                throw new System.ArgumentNullException(nameof(meshes));
+            else if (transforms == null)
+                throw new System.ArgumentNullException(nameof(transforms));
+            else if (materials == null)
+                throw new System.ArgumentNullException(nameof(materials));
+
+            Transform[] resultBones;
+            return CombineMeshes(meshes, transforms, materials, null, out resultMaterials, out resultBones);
+        }
+
+        /// <summary>
+        /// Combines an array of meshes into a single mesh.
+        /// </summary>
+        /// <param name="meshes">The array of meshes to combine.</param>
+        /// <param name="transforms">The array of transforms for the meshes.</param>
+        /// <param name="materials">The array of materials for each mesh to combine.</param>
+        /// <param name="bones">The array of bones for each mesh to combine.</param>
+        /// <param name="resultMaterials">The resulting materials for the combined mesh.</param>
+        /// <param name="resultBones">The resulting bones for the combined mesh.</param>
+        /// <returns>The combined mesh.</returns>
+        public static Mesh CombineMeshes(Mesh[] meshes, Matrix4x4[] transforms, Material[][] materials, Transform[][] bones, out Material[] resultMaterials, out Transform[] resultBones)
+        {
+            if (meshes == null)
+                throw new System.ArgumentNullException(nameof(meshes));
+            else if (transforms == null)
+                throw new System.ArgumentNullException(nameof(transforms));
+            else if (materials == null)
+                throw new System.ArgumentNullException(nameof(materials));
+            else if (transforms.Length != meshes.Length)
+                throw new System.ArgumentException("The array of transforms doesn't have the same length as the array of meshes.", nameof(transforms));
+            else if (materials.Length != meshes.Length)
+                throw new System.ArgumentException("The array of materials doesn't have the same length as the array of meshes.", nameof(materials));
+            else if (bones != null && bones.Length != meshes.Length)
+                throw new System.ArgumentException("The array of bones doesn't have the same length as the array of meshes.", nameof(bones));
+
+            int totalVertexCount = 0;
+            int totalSubMeshCount = 0;
+            for (int meshIndex = 0; meshIndex < meshes.Length; meshIndex++)
+            {
+                var mesh = meshes[meshIndex];
+                if (mesh == null)
+                    throw new System.ArgumentException(string.Format("The mesh at index {0} is null.", meshIndex), nameof(meshes));
+
+                totalVertexCount += mesh.vertexCount;
+                totalSubMeshCount = mesh.subMeshCount;
+
+                // Validate the mesh materials
+                var meshMaterials = materials[meshIndex];
+                if (meshMaterials == null)
+                    throw new System.ArgumentException(string.Format("The materials for mesh at index {0} is null.", meshIndex), nameof(materials));
+                else if (meshMaterials.Length != mesh.subMeshCount)
+                    throw new System.ArgumentException(string.Format("The materials for mesh at index {0} doesn't match the submesh count ({1} != {2}).", meshIndex, meshMaterials.Length, mesh.subMeshCount), nameof(materials));
+
+                for (int materialIndex = 0; materialIndex < meshMaterials.Length; materialIndex++)
+                {
+                    if (meshMaterials[materialIndex] == null)
+                        throw new System.ArgumentException(string.Format("The material at index {0} for mesh at index {1} is null.", materialIndex, meshIndex), nameof(materials));
+                }
+
+                // Validate the mesh bones
+                if (bones != null)
+                {
+                    var meshBones = bones[meshIndex];
+                    if (meshBones == null)
+                        throw new System.ArgumentException(string.Format("The bones for mesh at index {0} is null.", meshIndex), nameof(meshBones));
+
+                    for (int boneIndex = 0; boneIndex < meshBones.Length; boneIndex++)
+                    {
+                        if (meshBones[boneIndex] == null)
+                            throw new System.ArgumentException(string.Format("The bone at index {0} for mesh at index {1} is null.", boneIndex, meshIndex), nameof(meshBones));
+                    }
+                }
+            }
+
+            var vertices = new Vector3[totalVertexCount];
+            var indices = new int[totalSubMeshCount][];
+
+            // TODO: Implement the actual mesh combining here!
+
+            resultMaterials = null;
+            resultBones = null;
+            return null;
+        }
+        #endregion
+    }
+}
