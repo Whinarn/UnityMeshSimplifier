50b6290c4ada6e82569a5a82d2ab340d0e318257
Mattias Edlund <matte.edlund@gmail.com>
2019-07-07 22:20:44 +0900

    Removed the long obsolete KeepBorders property on the MeshSimplifier class.

--- Runtime/MeshSimplifier.cs
+++ Runtime/MeshSimplifier.cs
@@ -445,17 +445,6 @@ namespace UnityMeshSimplifier
         /// Default value: false
         /// </summary>
         [Obsolete("Use the 'MeshSimplifier.PreserveBorderEdges' property instead.", false)]
-        public bool KeepBorders
-        {
-            get { return this.PreserveBorderEdges; }
-            set { this.PreserveBorderEdges = value; }
-        }
-
-        /// <summary>
-        /// Gets or sets if the border edges should be preserved.
-        /// Default value: false
-        /// </summary>
-        [Obsolete("Use the 'MeshSimplifier.PreserveBorderEdges' property instead.", false)]
         public bool PreserveBorders
         {
             get { return this.PreserveBorderEdges; }
