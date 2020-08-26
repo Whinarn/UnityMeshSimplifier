2270d467ef54ade7fb64cf45f52fd8d31125b3b5
Mattias Edlund <matte.edlund@gmail.com>
2020-08-26 11:59:25 +0200

    ci(tests): fixed issues with running tests in 2020

--- .circleci/scripts/run_unity_tests.sh
+++ .circleci/scripts/run_unity_tests.sh
@@ -8,25 +8,42 @@ PROJECT_ROOT="$(pwd)"
 TEST_RESULT_DIR="$PROJECT_ROOT/test-results/nunit"
 TEST_RESULT_FILE_NAME="$TEST_PLATFORM-results.xml"
 TEST_RESULT_FILE_PATH="$TEST_RESULT_DIR/$TEST_RESULT_FILE_NAME"
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
-  -projectPath "$PROJECT_ROOT" \
-  -runTests \
-  -testPlatform "$TEST_PLATFORM" \
-  -testResults "$TEST_RESULT_FILE_PATH" \
-  -logFile - \
-  -batchmode
+
+if [ -z "$UNITY_EXECUTABLE" ]; then
+    UNITY_EXECUTABLE="/opt/Unity/Editor/Unity"
+fi
+
+if [ "$RUN_UNITY_WITH_NOGRAPHICS" == "true" ]; then
+    $UNITY_EXECUTABLE \
+        -projectPath "$PROJECT_ROOT" \
+        -runTests \
+        -testPlatform "$TEST_PLATFORM" \
+        -testResults "$TEST_RESULT_FILE_PATH" \
+        -logFile - \
+        -batchmode \
+        -nographics
+else
+    xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_EXECUTABLE \
+        -projectPath "$PROJECT_ROOT" \
+        -runTests \
+        -testPlatform "$TEST_PLATFORM" \
+        -testResults "$TEST_RESULT_FILE_PATH" \
+        -logFile - \
+        -batchmode
+fi
 
 UNITY_EXIT_CODE=$?
 
 if [ $UNITY_EXIT_CODE -eq 0 ]; then
-  echo "Run succeeded, no failures occurred";
+    echo "Run succeeded, no failures occurred";
 elif [ $UNITY_EXIT_CODE -eq 2 ]; then
-  echo "Run succeeded, some tests failed";
+    echo "Run succeeded, some tests failed";
 elif [ $UNITY_EXIT_CODE -eq 3 ]; then
-  echo "Run failure (other failure)";
+    echo "Run failure (other failure)";
 else
-  echo "Unexpected exit code $UNITY_EXIT_CODE";
+    echo "Unexpected exit code $UNITY_EXIT_CODE";
+    exit $UNITY_EXIT_CODE
 fi
 
 grep test-run < "$TEST_RESULT_FILE_PATH" | grep Passed
-exit $UNITY_TEST_EXIT_CODE
+exit $UNITY_EXIT_CODE
